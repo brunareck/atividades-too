b2e1a6427a1ffc5615043d49f94913a77e4244b8
Bruna Castelani <reck.brunac@gmai.com>
2025-05-28 10:14:54 -0300

    Criado a classe Gerente

---
+++ src/modelo/Gerente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+public class Gerente extends Funcionario {
+
+    @Override
+    public double calculaSalarioMes() {
+        return salario * 1.5;
+
+    }
+
+}
